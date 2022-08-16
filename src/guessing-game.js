ba85a646f7291556698a552901a126860df632d4
predmaxim <predmaxim@ya.ru>
2022-08-17 01:32:51 +0500

    feat: all tests is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,19 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.root = null
+        this.max = null
+        this.min = null
+    }
     setRange(min, max) {
-
+        this.max = max
+        this.min = min
     }
-
     guess() {
-
+        this.root = Math.round((this.min + this.max) / 2)
+        return this.root
     }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    lower() { this.max = this.root }
+    greater() { this.min = this.root }
 }
-
-module.exports = GuessingGame;
+module.exports = GuessingGame
